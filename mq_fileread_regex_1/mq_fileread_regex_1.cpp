8f7b198f8618f6006bd631a84d6dbb9f79a7de30
Leonid133 <lenin133@yandex.ru>
2015-11-08 23:23:08 +0300

    Добавил описание в начале работы программы

--- mq_fileread_regex_1/mq_fileread_regex_1.cpp
+++ mq_fileread_regex_1/mq_fileread_regex_1.cpp
@@ -14,14 +14,32 @@ int _tmain( int argc, _TCHAR* argv[] )
     if( 3 == argc ) 
     {
         //testfile.txt **ОЛГ?Р**
+        int filename_len  = _tcslen( argv[1] );
+        if(filename_len > 100)
+        {
+            printf( "\nИмя файла слишком большое\n");
+            return 1;
+        }
         szFileName = argv[1];
         int filter_len  = _tcslen( argv[2] );
+        if(filter_len > 100)
+        {
+            printf( "\nРегулярное выражение слишком большое\n");
+            return 1;
+        }
         filter = new char[filter_len+1];
         WideCharToMultiByte( CP_ACP, 0, argv[2], -1, filter, filter_len, NULL, NULL );
         filter[filter_len] = '\0';
     }
     else
     {
+        printf( "\nПример: .exe filename.txt regexp\n" );
+        printf( "\n.* - последовательность любых символов неограниченной длины\n" );
+        printf( "\n. - один любой символ\n" );
+        printf( "\n? - Ноль или одно	{0,1}	colou?r	color, colour\n" );
+        printf( "\n* - Ноль или более	{0,}	colou*r	color, colour, colouur и т. д.\n" );
+        printf( "\n+ - Одно или более	{1,}	colou+r	colour, colouur и т. д. (но не color)\n" );
+        printf( "\nИспользуем значения по умолчанию\n" );
         szFileName = L"testfile.txt";
         
         filter = new char[100];
@@ -47,8 +65,9 @@ int _tmain( int argc, _TCHAR* argv[] )
         //filter = "КА+ЗАБ?УЛ.*ЬК*А";
          //filter = "КАЗАБ?ЛЬКА";
         //filter = "КАЗАБ.ЛЬКА";
-         filter = "КАЗАБУЛЬК*А";
+         //filter = "КАЗАБУЛЬК*А";
         //filter = "КАЗАБУЛ.*ЬКА";
+        filter = "КА*ЗАБУЛ.*ЬКА";
         // filter = ".*.*КАЗАБУЛЬКА";
     }
     printf("Будет произведен поиск %s в файле ", filter);
@@ -90,7 +109,7 @@ int _tmain( int argc, _TCHAR* argv[] )
     printf( "\nПоиск произведен за %d мс\n", tick_end-tick_start_regex );
     printf( "\nРегулярное выражение %s \n", filter );
     printf( "\nНайдено %d строчек\n", find_counter );
-
+    printf( "\nНажмите любую клавишу, для завершения программы\n");
     _getch();
 	
     return 0;
